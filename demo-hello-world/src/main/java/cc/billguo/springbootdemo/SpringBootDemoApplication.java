5af6e296307e32c148b9a28913c58756785c9e15
billguocs <billguocs@gmail.com>
2023-08-09 23:14:06 +0800

    add: flyway module

--- demo-hello-world/src/main/java/cc/billguo/springbootdemo/SpringBootDemoApplication.java
+++ demo-hello-world/src/main/java/cc/billguo/springbootdemo/SpringBootDemoApplication.java
@@ -1,4 +1,4 @@
-package com.billguo.springbootdemo;
+package cc.billguo.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
